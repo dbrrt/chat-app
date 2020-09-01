86a12fef28c029f4d496fa00ccc443a1238c393b
David Barrat <david@macbook-pro-de-david.home>
2020-09-02 01:36:46 +0200

    config: adding scss support

--- chat-ui/rollup.config.js
+++ chat-ui/rollup.config.js
@@ -6,24 +6,22 @@ import external from 'rollup-plugin-peer-deps-external'
 import filesize from 'rollup-plugin-filesize'
 import resolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
+import scss from 'rollup-plugin-scss'
 
 export default {
   input: './src/index.tsx',
-  // external: ['react'],
   output: {
     name: 'bundle',
     file: './dist/bundle.js',
     format: 'cjs',
     sourcemap: true,
-    globals: {
-      // react: 'React',
-    }
   },
   plugins: [
     replace({
       'process.env.NODE_ENV': JSON.stringify( 'production' )
     }),
     typescript(),
+    scss(),
     external(),
     commonjs({
       include: ['node_modules/**'],
--- chat-ui/src/App/MessageTile/MessageTile.tsx
+++ chat-ui/src/App/MessageTile/MessageTile.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import {IMessageTile} from '../index.d'
+import './style.scss'
 
 export const MessageTile = ({timestamp, username, message}: IMessageTile) => {
-    return <>
+    return <div className='message-tile'>
         {username}  {timestamp} {JSON.stringify(message)}
-    </>
+        {/* <MessageTileStyled /> */}
+    </div>
 }
