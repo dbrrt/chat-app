56c04d19ff51c73c05918557dfcd088c8731e72f
David Barrat <david@barrat.io>
2020-09-02 17:59:15 +0200

    ui cleanup/adding animations

--- chat-ui/src/App/NewMessageBox/index.tsx
+++ chat-ui/src/App/NewMessageBox/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment } from '@fortawesome/free-solid-svg-icons'
+import { faComment, faTimes } from '@fortawesome/free-solid-svg-icons'
 import './style.scss'
 export const NewMessageBox = () => {
     return (
         <div className='message-box'>
+            <FontAwesomeIcon className='close-icon-btn' icon={faTimes}/>
             <div>
-                <textarea rows={1} className='input-message-box' />
+                <textarea rows={3} className='input-message-box' />
                 <br />
                 <br />
                 <button className='input-message-btn'>Send Message <FontAwesomeIcon icon={faComment}/></button>
--- chat-ui/src/App/index.tsx
+++ chat-ui/src/App/index.tsx
@@ -10,78 +10,78 @@ const messages = [
             payload: 'Hello'
         }
     },
-    {
-        sender: 'guest001',
-        recipient: 'guest002',
-        message: {
-            type: 'text',
-            payload: 'How are you?'
-        }
-    },
-    {
-        sender: 'guest002',
-        recipient: 'guest001',
-        message: {
-            type: 'text',
-            payload: 'Hello'
-        }
-    },
-    {
-        sender: 'guest001',
-        recipient: 'guest002',
-        message: {
-            type: 'text',
-            payload: 'How are you?'
-        }
-    },
-    {
-        sender: 'guest002',
-        recipient: 'guest001',
-        message: {
-            type: 'text',
-            payload: 'Hello'
-        }
-    },
-    {
-        sender: 'guest002',
-        recipient: 'guest001',
-        message: {
-            type: 'text',
-            payload: 'Hello'
-        }
-    },
-    {
-        sender: 'guest001',
-        recipient: 'guest002',
-        message: {
-            type: 'text',
-            payload: 'How are you?'
-        }
-    },
-    {
-        sender: 'guest002',
-        recipient: 'guest001',
-        message: {
-            type: 'text',
-            payload: 'Hello'
-        }
-    },
-    {
-        sender: 'guest001',
-        recipient: 'guest002',
-        message: {
-            type: 'text',
-            payload: 'How are you?'
-        }
-    },
-    {
-        sender: 'guest002',
-        recipient: 'guest001',
-        message: {
-            type: 'text',
-            payload: 'Hello'
-        }
-    },
+    // {
+    //     sender: 'guest001',
+    //     recipient: 'guest002',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'How are you?'
+    //     }
+    // },
+    // {
+    //     sender: 'guest002',
+    //     recipient: 'guest001',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'Hello'
+    //     }
+    // },
+    // {
+    //     sender: 'guest001',
+    //     recipient: 'guest002',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'How are you?'
+    //     }
+    // },
+    // {
+    //     sender: 'guest002',
+    //     recipient: 'guest001',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'Hello'
+    //     }
+    // },
+    // {
+    //     sender: 'guest002',
+    //     recipient: 'guest001',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'Hello'
+    //     }
+    // },
+    // {
+    //     sender: 'guest001',
+    //     recipient: 'guest002',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'How are you?'
+    //     }
+    // },
+    // {
+    //     sender: 'guest002',
+    //     recipient: 'guest001',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'Hello'
+    //     }
+    // },
+    // {
+    //     sender: 'guest001',
+    //     recipient: 'guest002',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'How are you?'
+    //     }
+    // },
+    // {
+    //     sender: 'guest002',
+    //     recipient: 'guest001',
+    //     message: {
+    //         type: 'text',
+    //         payload: 'Hello'
+    //     }
+    // },
 ]
 
 export const App = () => <Container messages={messages} />
